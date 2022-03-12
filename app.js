6b9582c94aa62084a4de380ab1985a3f266f90c4
Federico <perezfedericoluis@gmail.com>
2022-03-11 21:30:39 -0300

    CRUD en Javascript y PHP

---
+++ app.js
@@ -0,0 +1,103 @@
+
+    let url = 'http://localhost/empleados/';
+    let modal= new bootstrap.Modal(document.getElementById('modelId'), {keyboard: false});
+
+    let app = new function() {
+        
+        this.idEditar=document.getElementById("idEditar");
+        this.nombreEditar=document.getElementById("nombreEditar");
+        this.correoEditar=document.getElementById("correoEditar");
+
+        this.nombre=document.getElementById("nombre");
+        this.correo=document.getElementById("correo");
+
+        this.empleados=document.getElementById("empleados");
+
+        this.leer = function(){
+            let datos="";
+
+            fetch(url)
+            .then(r=>r.json())
+            .then((respuesta)=>{
+                respuesta.map(
+                    function(empleado,index,array){
+
+                        datos+="<tr>";
+
+                        datos+="<td>"+empleado.id+"</td>";
+                        datos+="<td>"+empleado.nombre+"</td>";
+                        datos+="<td>"+empleado.correo+"</td>";
+                        datos+='<td><div class="btn-group" role="group" aria-label=""><button type="button" class="btn btn-success" onclick="app.editar('+empleado.id+')">Editar</button><button type="button" class="btn btn-danger" onclick="app.borrar('+empleado.id+')">Borrar</button></button></div>'+'</td>';
+                        datos+="</tr>";
+                    }
+                );
+                return this.empleados.innerHTML=datos;
+            })
+            .catch(console.log);
+            
+            
+        };
+
+        this.agregar = function(){
+
+            console.log(nombre.value);
+            console.log(correo.value);
+
+            let datosEnviar= {nombre:this.nombre.value, correo:this.correo.value};
+
+            fetch(url+"?insertar=1",{method:"POST", body:JSON.stringify(datosEnviar)})
+            .then((respuesta)=>respuesta.json())
+            .then((datosRespuesta)=>{
+
+                console.log("Insertados");
+                this.leer();
+            })
+            .catch(console.log);
+    };
+
+    this.borrar=function(id){
+        console.log(id);
+
+        fetch(url+"?borrar="+id)
+            .then((respuesta)=>respuesta.json())
+            .then((datosRespuesta)=>{
+
+                this.leer();
+            })
+            .catch(console.log);
+    };
+
+    this.editar=function(id){
+        console.log(id);
+
+        fetch(url+"?consultar="+id)
+            .then((respuesta)=>respuesta.json())
+            .then((datosRespuesta)=>{
+                console.log(datosRespuesta);
+
+                this.nombreEditar.value=datosRespuesta[0]['nombre'];
+                this.idEditar.value=datosRespuesta[0]['id'];
+                this.correoEditar.value=datosRespuesta[0]['correo'];
+            })
+            .catch(console.log);
+        modal.show();
+    };
+
+    this.actualizar=function(){
+
+        let datosEnviar= {id:this.idEditar.value, nombre:this.nombreEditar.value, correo:this.correoEditar.value};
+
+        fetch(url+"?actualizar=1",{method:"POST", body:JSON.stringify(datosEnviar)})
+            .then((respuesta)=>respuesta.json())
+            .then((datosRespuesta)=>{
+
+                console.log("actualizados");
+                this.leer();
+                modal.hide();
+            })
+            .catch(console.log);
+       
+    };
+}
+
+    app.leer();
